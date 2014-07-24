d02b824b05f344cbeeb603ef727f4c4460f4bc89
Baargav Venkatesh <kon7341@ex.kroger.com>
2014-07-23 23:14:44 -0400

    Added increment vote Method..

--- routes/index.js
+++ routes/index.js
@@ -29,7 +29,7 @@ router.post('/articles',function(req,res){
 		"title": title,
 		"votes": 0,
 		"date": currentTime,
-		"score":2.0,
+		"score":1.0,
 		"type":"link"
 	},function(err,doc){
 		if(err){
@@ -42,45 +42,47 @@ router.post('/articles',function(req,res){
 
 });
 
-/* GET Default mapping*/
-router.get('/', function(req, res) {
-  res.send('alive');
-});
 
-/*POST links to newsDB */
-router.post('/addarticle',function(req,res){
-
+/*POST increment vote count */
+router.post('/vote',function(req,res){
 	var db = req.db;
-
-	var url = req.body.url;
-	var title = req.body.title;
-	var votes = req.body.votes;
+	var votedUrl = req.body.url;
 	var currentTime = new Date();
 
-	var collection = db.get('usercollection');
+	res.setHeader('Content-Type', 'application/json');
 
-	collection.insert({
-		"url" : url,
-		"title": title,
-		"votes": 0,
-		"date": currentTime,
-		"score":2,
-		"type":"link"
-	},function(err,doc){
+	var collection = db.get('usercollection');
+	collection.update({
+	 	"url": votedUrl 
+	 			},
+	   {
+      	$inc: { votes: 1 }
+   		},
+  	   { upsert: true }
+  	   ,function(err,doc){
 		if(err){
-			res.send("post to db failed");
+			res.send("Post Failed");
 		}
 		else{
-			res.location("articles");
-			res.redirect("articles")
+			res.json();
 		}
 	});
 
 });
 
+
+
+
+/* GET Default mapping*/
+router.get('/', function(req, res) {
+  res.send('alive');
+});
+
+
 /*Add links for articles from newsDB*/
 router.get('/newarticle', function(req, res) {
     res.render('newarticle', { title: 'Add New Article' });
 });
+
 
 module.exports = router;
