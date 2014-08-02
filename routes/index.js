c366a06c875cb71607f524a38f7995d323d73e94
Baargav Venkatesh <kon7341@ex.kroger.com>
2014-08-01 23:51:45 -0400

    NOJIRA. Added a function to clean urls.

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,10 @@ var router = express.Router();
 router.get('/articles', function(req, res) {
     var db = req.db;
     var collection = db.get('usercollection');
+
     collection.find({type:"link"},{sort:[["score","desc"]]},function(e,docs){
-        res.render('articles', {
-            "articles" : docs
-        });
+	res.setHeader('Content-Type', 'application/json');
+    res.json(docs);
     });
 });
 
@@ -17,8 +17,9 @@ router.get('/articles', function(req, res) {
 router.post('/articles',function(req,res){
 
 	var db = req.db;
-	var url = req.body.url;
-	var title = req.body.title;
+	var url = cleanUrl(req.body.gist.path);
+	var title = req.body.gist.name;
+	var content = req.body.gist.content
 	var currentTime = new Date();
 	var collection = db.get('usercollection');
 
@@ -46,17 +47,23 @@ router.post('/articles',function(req,res){
 /*POST increment vote count */
 router.post('/vote',function(req,res){
 	var db = req.db;
-	var votedUrl = req.body.url;
+	var votedUrl = cleanUrl(req.body.url);
 	var currentTime = new Date();
 
 	res.setHeader('Content-Type', 'application/json');
+	//find
 
 	var collection = db.get('usercollection');
+	var doc = collection.find({"url": votedUrl});
+	console.log(doc[0]);
+	var newScore = calculateScore(doc.score, 1)
+
 	collection.update({
 	 	"url": votedUrl 
 	 			},
 	   {
-      	$inc: { votes: 1 }
+      	$inc: {votes: 1 },
+      	$set: {score: newScore}
    		},
   	   { upsert: true }
   	   ,function(err,doc){
@@ -84,5 +91,13 @@ router.get('/newarticle', function(req, res) {
     res.render('newarticle', { title: 'Add New Article' });
 });
 
+ var calculateScore = function(votes,score){
+       return score+votes;
+}
 
+var cleanUrl = function(url) {
+	/*this regular expression removes protocols(eg. https) from urls*/
+		return url.replace(/.*?:\/\//g, "");
+}
+/*number of votes**/
 module.exports = router;
